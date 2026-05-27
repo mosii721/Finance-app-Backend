c955c83e8e4c8ec7c1c90910087eeb65dded1baa
mosii721 <mikealwanga97@gmail.com>
2026-05-27 14:47:01 +0300

    added exception filters

---
+++ src/http-exception.filter.ts
@@ -0,0 +1,60 @@
+import { ArgumentsHost, Catch, HttpException, HttpStatus } from "@nestjs/common";
+import { BaseExceptionFilter } from "@nestjs/core";
+import { Request, response } from "express";
+
+interface MyResponseObj{
+    statusCode: number,
+    timestamp: string,
+    path: string,
+    response: string | object,
+}
+
+@Catch()
+export class AllExecptionFilters extends BaseExceptionFilter{
+    private getClientIp(request:Request) {
+        const forwardedFor = request.headers['x-forwarded-for']
+
+        if(forwardedFor) {
+            return Array.isArray(forwardedFor)
+                ? forwardedFor[0]
+                : forwardedFor.split(',')[0].trim()
+        }
+
+        return request.ip || 'Unknown'
+    }
+
+    catch(exception: any, host: ArgumentsHost){
+        const ctx = host.switchToHttp()
+        const request = ctx.getRequest<Request>()
+        const clientIp = this.getClientIp(request)
+
+        const myResponseObj: MyResponseObj = {
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            response: '',
+        }
+
+        if(exception instanceof HttpException) {
+            myResponseObj.statusCode = exception.getStatus()
+            myResponseObj.response = exception.getResponse()
+        } else if(exception instanceof Error) {
+            myResponseObj.response = exception.message
+        } else {
+            myResponseObj.response = 'Internal Server Error'
+        }
+
+        response.status(myResponseObj.statusCode).json(myResponseObj)
+
+        const logMessage = typeof myResponseObj.response === 'string'
+                                ? myResponseObj.response
+                                : JSON.stringify(myResponseObj.response)
+
+        const fullLog = `[ERROR] ${myResponseObj.timestamp} | Client Ip ${clientIp} | Path ${request.url} | Message ${logMessage}`
+        console.error(fullLog)
+
+        if(exception instanceof Error) {
+            console.error('Stack trace', exception.stack)
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExecptionFilters } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExecptionFilters(httpAdapter))
   app.useGlobalPipes( new ValidationPipe({ whitelist:true }))
+  
   const configService = app.get(ConfigService);
   const PORT = configService.getOrThrow<number>('PORT');
   await app.listen(PORT, '0.0.0.0');
